4a10a237896904dc403dd283cb9419195bcb3d5a
user-victoria <vicky.t.ma.27@gmail.com>
2023-09-22 18:55:00 +0200

    homework 1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// За допомогою prompt запитати ім'я користувача
+let userName = prompt('What is your name?');
+
+// За допомогою alert вивести "Hello, John! How are you?", де John це те, що ввів користувач
+alert (`Hello ${userName}! How are you?`);
